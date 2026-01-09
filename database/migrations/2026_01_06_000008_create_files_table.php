55ef58f4e7c5a123661f45bc620baaa3664b3111
pikio <wibivicky@gmail.com>
2026-01-09 21:38:27 +0700

    add new model and migration file

---
+++ database/migrations/2026_01_06_000008_create_files_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('file_name');
+            $table->string('file_url');
+            $table->string('file_path');
+            $table->string('file_type');
+            $table->bigInteger('file_size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
