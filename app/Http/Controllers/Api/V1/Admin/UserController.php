5ed02b6ae707260a9412860f97ba2b6aa9eaa5f9
pikio <wibivicky@gmail.com>
2026-01-23 00:26:24 +0700

    remove comment in controller

--- app/Http/Controllers/Api/V1/Admin/UserController.php
+++ app/Http/Controllers/Api/V1/Admin/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\V1\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class SocialAssistanceController extends Controller
+class UserController extends Controller
 {
     public function index()
     {
