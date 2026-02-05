625b8e5660bda75874545de1cecbaf2ff0f3a9ae
pikio <wibivicky@gmail.com>
2026-02-05 23:16:57 +0700

    update add date immutable on service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Date;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureDefault();
+    }
+
+    protected function configureDefault(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
