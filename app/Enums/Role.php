874581f9c1c2ed23050d58ac40c38c08d8baead4
pikio <wibivicky@gmail.com>
2026-02-12 23:58:39 +0700

    add new string role Head Village

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -7,11 +7,13 @@ enum Role: string
     case Admin = 'admin';
     case HeadOfFamily = 'head_of_family';
     case FamilyMember = 'family_member';
+    case HeadVillage = 'head_village';
 
     public function label(): string
     {
         return match($this) {
             self::Admin => 'Administrator',
+            self::HeadVillage => 'Kepala Desa',
             self::HeadOfFamily => 'Kepala Keluarga',
             self::FamilyMember => 'Anggota Keluarga',
         };
