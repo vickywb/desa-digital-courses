da981c6c5f494010069841bcb0fcea9e1fc63fcd
pikio <wibivicky@gmail.com>
2026-02-13 00:02:22 +0700

    add userfactory for test case

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => 'head_of_family',
             'remember_token' => Str::random(10),
         ];
     }
