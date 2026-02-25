acabc365e81800b7489979c3dc7c15aadbf31a51
pikio <wibivicky@gmail.com>
2026-02-25 17:32:58 +0700

    update: add family member id on event participant seeder

--- database/seeders/EventParticipantSeeder.php
+++ database/seeders/EventParticipantSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Event;
 use Illuminate\Database\Seeder;
 use App\Models\EventParticipant;
+use App\Models\FamilyMember;
 use App\Models\HeadOfFamily;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -17,12 +18,14 @@ class EventParticipantSeeder extends Seeder
     {
         $events = Event::all();
         $headOfFamilies = HeadOfFamily::all();
+        $familyMember = FamilyMember::all();
 
         foreach ($events as $event) {
             foreach ($headOfFamilies as $headOfFamily) {
                 EventParticipant::create([
                     'event_id' => $event->id,
                     'head_of_family_id' => $headOfFamily->id,
+                    'family_member_id' => $familyMember->random()->id,
                     'quantity' => 2,
                     'total_price' => 20000,
                     'payment_status' => 'paid',
