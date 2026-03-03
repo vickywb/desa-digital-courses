93f38173881ee194c0d40164cc997b1788435999
pikio <wibivicky@gmail.com>
2026-03-04 01:32:29 +0700

    update: auth service add new function, login, logout and Me

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Repository\UserRepository;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AuthService
 {
@@ -32,7 +33,7 @@ class AuthService
             try {
                 // 1. Upload profile picture (if exists)
                 if ($profilePicture) {
-                    $fileData = $this->fileService->uploadFile($profilePicture, 'profile-photos');
+                    $fileData = $this->fileService->uploadFile($profilePicture, 'file/profile-photos');
                     $uploadedFileId = $fileData['id'];
                 }
 
@@ -83,5 +84,72 @@ class AuthService
                 throw $th;
             }
         });
+    }
+
+    public function login(string $identifier, string $password): array
+    {
+        // Find user by username or identity number
+        $user = User::where('username', $identifier)
+            ->orWhereHas('headOfFamily', function ($query) use ($identifier) {
+                $query->where('identity_number', $identifier);
+            })->first();
+        
+        // Validate User Exists
+        if (!$user) {
+            LoggerHelper::warning('Login Failed. Incorrect userename or password', [
+                'identifier' => $identifier,
+            ]);
+
+            throw new \Exception('Username or password is incorrect.');
+        }
+
+        // Validate Password
+        if (!Hash::check($password, $user->password)) {
+            LoggerHelper::warning('Login Failed. Incorrect userename or password', [
+                'identifier' => $identifier,
+                'user_id' => $user->id,
+            ]);
+
+            throw new \Exception('Login Failed. Username or password is incorrect.');
+        }
+
+        // Generate token
+        $token = $user->createToken('auth_token', [$user->role])->plainTextToken;
+        
+        // Log success
+        LoggerHelper::info('User Successfully Logged In.', [
+            'token' => substr($token, 0, 5) . '...' . substr($token, -5),
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username,
+            ],
+        ]);
+        
+        // Load relationships
+        $user->load('headOfFamily.file');
+        
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
+    }
+
+    public function logout(User $user): bool
+    {
+        $user->currentAccessToken()->delete();
+
+        LoggerHelper::info('User Successfully Logged Out.', [
+            'user_id' => $user->id,
+        ]);
+
+        return true;
+    }
+
+    /**
+     * Get authenticated user.
+     */
+    public function me(User $user): User
+    {
+        return $user->load('headOfFamily.file');
     }
 }
