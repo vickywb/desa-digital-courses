29a2736dac594290a0298eec1114473f5faaed68
pikio <wibivicky@gmail.com>
2026-03-10 20:09:29 +0700

    update: fixing naming in family relation enum

--- app/Enums/FamilyRelation.php
+++ app/Enums/FamilyRelation.php
@@ -6,14 +6,14 @@ enum FamilyRelation: string
 {
     Case Child = 'child';
     Case Wife = 'wife';
-    Case Husband = 'husband';
+    Case HeadFamily = 'head';
 
     public function label(): string
     {
         return match($this) {
             self::Child => 'Anak',
             self::Wife => 'Istri',
-            self::Husband => 'Suami',
+            self::HeadFamily => 'Kepala Keluarga',
         };
     }
 }
