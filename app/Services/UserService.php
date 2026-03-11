75690ac8a991ba28fda731c1e274b9e961f41139
pikio <wibivicky@gmail.com>
2026-03-11 23:26:19 +0700

    update: add function for check specific role and add limit on it

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,14 +2,44 @@
 
 namespace App\Services;
 
-use App\Repository\UserRepository;
+use App\Enums\Role;
+use App\Helpers\LoggerHelper;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserService
 {
-    private $userRepository;
-
-    public function __construct(UserRepository $userRepository)
+    public function updateUserRole(User $user, Role $newRole): User
     {
-        $this->userRepository = $userRepository;
+        // Cek kuota staff
+        $this->maxQuotaForRole($user, $newRole);
+
+        return DB::transaction(function () use ($user, $newRole) {
+            $user->update(['role' => $newRole->value]);
+            return $user->fresh();
+
+            LoggerHelper::info('User role updated successfully.', [
+                'user_id'  => $user->id,
+                'new_role' => $newRole->value,
+            ]);
+        });
     }
+
+    // Cek kuota untuk role tertentu
+    public function maxQuotaForRole(User $user, Role $newRole): void
+    {
+        $maxQuota = $newRole->maxQuota();
+
+        if ($maxQuota === null) return;
+
+        $currentQuota = User::where('role', $newRole->value)
+            ->where('id', '!=', $user->id)
+            ->count();
+        
+        if ($currentQuota >= $maxQuota) {
+            throw new \Exception("Quota for role {$newRole->value} has been reached.");
+        }
+    }
+
+
 }
