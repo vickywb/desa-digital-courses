ba0da3b61d4f2010e7f472f3537b62e45199d678
pikio <wibivicky@gmail.com>
2026-03-11 08:02:38 +0700

    update: set family member id to nullable,  default quantity to 1, and change string to enum for payment status

--- database/migrations/2026_01_07_022021_create_event_participants_table.php
+++ database/migrations/2026_01_07_022021_create_event_participants_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->foreignUuid('event_id')->constrained('events')->onUpdate('cascade')->onDelete('cascade'); //Relasi ke table events
             $table->foreignUuid('head_of_family_id')->constrained('head_of_families')->onUpdate('cascade')->onDelete('cascade'); //Relasi ke table head of families
-            $table->foreignUuid('family_member_id')->constrained('family_members')->onUpdate('cascade')->onDelete('cascade'); //Relasi ke table family members
-            $table->integer('quantity')->default(0);
+            $table->foreignUuid('family_member_id')->nullable()->constrained('family_members')->onUpdate('cascade')->onDelete('cascade'); //Relasi ke table family members
+            $table->integer('quantity')->default(1);
             $table->decimal('total_price', 16, 2)->default(0);
-            $table->string('payment_status')->default('pending');
+            $table->enum('payment_status', ['free', 'pending', 'paid'])->default('pending');
             $table->softDeletes();
             $table->timestamps();
         });
