db956dd060b431552774d2829a5ad445c1e422be
pikio <wibivicky@gmail.com>
2026-03-16 09:48:43 +0700

    update: user controller on admin panel

--- app/Http/Controllers/Api/V1/Admin/UserController.php
+++ app/Http/Controllers/Api/V1/Admin/UserController.php
@@ -2,19 +2,32 @@
 
 namespace App\Http\Controllers\Api\V1\Admin;
 
+use App\Enums\Role;
+use App\Helpers\LoggerHelper;
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\UserUpdateRequest;
+use App\Http\Resources\UserColletcion;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Repository\UserRepository;
+use App\Services\UserService;
 
 class UserController extends Controller
 {
+    public function __construct(
+        private UserService $userService,
+        private UserRepository $userRepository
+    )
+    {}
+
     public function index()
     {
-        //
-    }
+        $users = $this->userRepository->get([
+            'with' => ['headOfFamily']
+        ]);
 
-    public function store(Request $request)
-    {
-        //
+        return ResponseHelper::success('Users retrived successfully', new UserColletcion($users), 200);
     }
 
     public function show(string $id)
@@ -22,13 +35,33 @@ class UserController extends Controller
         //
     }
 
-    public function update(Request $request, string $id)
+    public function update(UserUpdateRequest $request, User $user)
     {
-        //
-    }
+        try {
+            $updatedUser = $this->userService->updateUserRole(
+                $user,
+                Role::from($request->validated('role'))
+            );
 
-    public function destroy(string $id)
-    {
-        //
+            LoggerHelper::info('User role updated successfully.', [
+                'user_id'  => $user->id,
+                'new_role' => $updatedUser->role->value,
+            ]);
+
+            return ResponseHelper::success(
+                'User updated successfully',
+                new UserResource($updatedUser),
+                200
+            );
+
+        } catch (\Throwable $e) {
+            
+            LoggerHelper::error('Failed to update user role.', [
+                'error'   => $e->getMessage(),
+                'user_id' => $user->id,
+            ]);
+
+            return ResponseHelper::error('Failed to update user role.', null, 500);
+        }
     }
 }
