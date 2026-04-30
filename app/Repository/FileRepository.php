67b8adca7f5355190cf159b8932dbbe49864898a
pikio <wibivicky@gmail.com>
2026-04-30 21:13:15 +0700

    chore: update file repository

--- app/Repository/FileRepository.php
+++ app/Repository/FileRepository.php
@@ -8,47 +8,22 @@ class FileRepository
 {
     public function __construct(private File $file) {}
 
-    /**
-     * Create file info ke database.
-     *
-     * @param array $fileData
-     * @return File
-     */
     public function create(array $fileData): File
     {
         return $this->file->create($fileData);
     }
 
-    /**
-     * Save file info ke database.
-     *
-     * @param  File $file
-     * @return File
-     */
     public function save(File $file): File
     {
         $file->save();
-
         return $file->fresh();
     }
-
-    /**
-     * Find file by ID.
-     *
-     * @param  int  $id
-     * @return File|null
-     */
+    
     public function findById(int $id): ?File
     {
         return $this->file->find($id);
     }
-
-    /**
-     * Delete file from database.
-     *
-     * @param File $file
-     * @return bool
-     */
+    
     public function delete(File $file): bool
     {
         return $file->delete();
