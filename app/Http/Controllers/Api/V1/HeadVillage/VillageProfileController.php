49b6eaa867869623d54e33102bc1bd600770a3d2
pikio <wibivicky@gmail.com>
2026-05-08 00:38:59 +0700

    chore: update village profile controller

--- app/Http/Controllers/Api/V1/HeadVillage/VillageProfileController.php
+++ app/Http/Controllers/Api/V1/HeadVillage/VillageProfileController.php
@@ -2,33 +2,48 @@
 
 namespace App\Http\Controllers\Api\V1\HeadVillage;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\VillageProfileStoreRequest;
+use App\Http\Requests\VillageProfileUpdateRequest;
+use App\Http\Resources\VillageProfileResource;
+use App\Models\VillageProfile;
+use App\Services\VillageProfileService;
 use Illuminate\Http\Request;
 
 class VillageProfileController extends Controller
 {
+    public function __construct(private VillageProfileService $villageProfileService){}
+
     public function index()
     {
         //
     }
 
-    public function store(Request $request)
+    public function store(VillageProfileStoreRequest $request)
     {
-        //
+        $villageProfile = $this->villageProfileService->createVillageProfile($request->validated(), $request->file('images', []));
+
+        return ResponseHelper::success('Village Profile successfully created', [
+        new VillageProfileResource($villageProfile)
+        ], 201);
     }
 
-    public function show(string $id)
+    public function update(VillageProfileUpdateRequest $request, VillageProfile $villageProfile)
     {
-        //
+        $villageProfile = $this->villageProfileService->updateVillageProfile($villageProfile, $request->validated(), $request->file('images', []));
+
+        return ResponseHelper::success('Village Profile successfully updated', [
+            new VillageProfileResource($villageProfile)
+        ], 200);
     }
 
-    public function update(Request $request, string $id)
+    public function destroy(VillageProfile $villageProfile)
     {
-        //
-    }
+        $villageProfile = $this->villageProfileService->deleteVillageProfile($villageProfile);
 
-    public function destroy(string $id)
-    {
-        //
+        return ResponseHelper::success('Village Profile successfully deleted', [
+            new VillageProfileResource($villageProfile)
+        ], 200);
     }
 }
