f1b361eb5a2236010ae6f1d1f8128c8251ac9c4e
pikio <wibivicky@gmail.com>
2026-05-08 19:37:00 +0700

    feat: update event service create, update and delete

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -2,27 +2,110 @@
 
 namespace App\Services;
 
+use App\Helpers\LoggerHelper;
 use App\Models\Event;
 use App\Repository\EventRepository;
+use App\Services\FileService;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
 
 class EventService
 {
-    // private $eventRepository;
+    public function __construct(
+        private EventRepository $eventRepository,
+        private FileService $fileService
+    ) {}
 
-    // public function __construct(EventRepository $eventRepository) {
-    //     $this->eventRepository = $eventRepository;
-    // }
+    public function createEvent (array $data, ?UploadedFile $eventFile = null): Event
+    {
+        try {
+            return DB::transaction(function() use ($data, $eventFile) {
+                $imageId = $this->fileService->handleUploadAndSave($eventFile, 'file/events')?->id;
 
-    // public function createSrvice (array $data, ?UploadedFile $file = null): Event 
-    // {
-    //     try {
+                $event = $this->eventRepository->save(new Event([
+                    ...$data,
+                    'file_id' => $imageId,
+                ]));
 
+                LoggerHelper::info('Event created successfully', [
+                    'event_id' => $event->id,
+                    'title' => $event->title,
+                ]);
+                
+                return $event->fresh();
+            });
 
-    //     } catch (\Throwable $th) {
+        } catch (\Throwable $th) {
+            LoggerHelper::error('Failed to create event', 
+                ['error_message' => $th->getMessage()]
+            );
 
+            throw $th;
+        }
+    }
 
-    //     }
-    // }
+    public function updateEvent(array $data, ?UploadedFile $eventFile, Event $event): Event
+    {
+        try {
+            return DB::transaction(function () use ($data, $eventFile, $event) {
+                $oldFileId = $event->file_id;
+                $newFileId = $eventFile
+                    ? $this->fileService->handleUploadAndSave($eventFile, 'file/events')?->id
+                    : $oldFileId;
+
+                $event->fill([
+                    ...$data, 
+                    'file_id' => $newFileId
+                ]);
+
+                $event = $this->eventRepository->save($event);
+
+                if ($eventFile && $oldFileId) {
+                    $this->fileService->deleteFile($oldFileId);
+                }
+
+                LoggerHelper::info('Event updated successfully', [
+                    'event_id' => $event->id,
+                    'title'    => $event->title,
+                ]);
+
+                return $event->fresh();
+            });
+
+            } catch (\Throwable $th) {
+                LoggerHelper::error('Failed to update event', [
+                    'event_id' => $event->id,
+                    'error_message' => $th->getMessage(),
+                ]);
+
+                throw $th;
+            }
+    }
+
+    public function deleteEvent(Event $event): void
+    {
+        try {
+            DB::transaction(function () use ($event) {
+                $fileId = $event->file_id;
+
+                $event->delete();
+
+                if ($fileId) {
+                    $this->fileService->deleteFile($fileId);
+                }
+
+                LoggerHelper::info('Event deleted successfully', [
+                    'event_id' => $event->id,
+                    'title' => $event->title,
+                ]);
+            });
+        } catch (\Throwable $th) {
+            LoggerHelper::error('Failed to delete event', [
+                'event_id' => $event->id,
+                'error_message' => $th->getMessage(),
+            ]);
+
+            throw $th;
+        }
+    }
 }
