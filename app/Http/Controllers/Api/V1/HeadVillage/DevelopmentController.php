9027fbbac947ec3d82eb147d6be4629229e0367a
pikio <wibivicky@gmail.com>
2026-05-09 17:39:35 +0700

    chore: update development controller

--- app/Http/Controllers/Api/V1/HeadVillage/DevelopmentController.php
+++ app/Http/Controllers/Api/V1/HeadVillage/DevelopmentController.php
@@ -2,19 +2,32 @@
 
 namespace App\Http\Controllers\Api\V1\HeadVillage;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DevelopmentStoreRequest;
+use App\Http\Requests\DevelopmentUpdateRequest;
+use App\Http\Resources\DevelopmentResource;
+use App\Models\Development;
+use App\Services\DevelopmentService;
 use Illuminate\Http\Request;
 
 class DevelopmentController extends Controller
 {
+    public function __construct(
+        private DevelopmentService $developmentService
+    ){}
+    
     public function index()
     {
         //
     }
 
-    public function store(Request $request)
+    public function store(DevelopmentStoreRequest $request)
     {
-        //
+       $development = $this->developmentService->createDevelopment($request->validated(), $request->file('image') ?? null);
+        return ResponseHelper::success('Development created successfully', [
+            new DevelopmentResource($development)
+        ], 201);
     }
 
     public function show(string $id)
@@ -22,13 +35,17 @@ class DevelopmentController extends Controller
         //
     }
 
-    public function update(Request $request, string $id)
+    public function update(DevelopmentUpdateRequest $request, Development $development)
     {
-        //
+        $this->developmentService->updateDevelopment($request->validated(), $request->file('image') ?? null, $development);
+        return ResponseHelper::success('Development updated successfully', [
+            new DevelopmentResource($development->fresh())
+        ], 200);
     }
 
-    public function destroy(string $id)
+    public function destroy(Development $development)
     {
-        //
+        $this->developmentService->deleteDevelopment($development);
+        return ResponseHelper::success('Development deleted successfully', null, 200);
     }
 }
