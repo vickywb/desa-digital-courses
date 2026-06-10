e7035e8929f3da1bf953f27b2a4ddee6dc3153f8
pikio <freakyfreakzz21@gmail.com>
2026-06-10 22:16:15 +0700

    chore: update role seeder

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -1,24 +1,25 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use App\Helpers\LoggerHelper;
+use App\Helpers\ResponseHelper;
 use Closure;
 use Illuminate\Http\Request;
-use App\Helpers\ResponseHelper;
 
 class RoleMiddleware
 {
     public function handle(Request $request, Closure $next, string ...$roles)
     {
         $user = $request->user();
-        
+
         // Cek user, apakah user terautentikasi dan role diizinkan
-        if (!$user || !in_array($user->role->value, $roles)) {
+        if (! $user || $user->is_active === false || ! in_array($user->role->value, $roles)) {
 
             // Log
             LoggerHelper::error('Access Denied', [
                 'user_id' => $user->id ?? 'guest',
-                'roles' => $user->role->value ?? 'guest'
+                'roles' => $user->role->value ?? 'guest',
             ]);
 
             return ResponseHelper::forbidden('Access Denied: Insufficient permissions.', 403);
