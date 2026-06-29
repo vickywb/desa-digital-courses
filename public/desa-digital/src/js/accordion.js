5e5c1999121d2b7f7af16643def36cf5ca1a3869
pikio <freakyfreakzz21@gmail.com>
2026-06-30 01:18:39 +0700

    chore: deleted unused file

--- public/desa-digital/src/js/accordion.js
+++
@@ -1,8 +0,0 @@
-$(() => {
-    $("[data-expand]").on("click", function (e) {
-        $(this).toggleClass("active");
-        $(this).children("img").toggleClass("rotate-180");
-        let target = $(`#${$(this).data("expand")}`);
-        target.slideToggle();
-    });
-});
