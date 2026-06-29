3e7a21151f4a7afb075b2020c0555dbee5d4c9db
pikio <freakyfreakzz21@gmail.com>
2026-06-29 20:58:53 +0700

    chore: update user repository

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -20,31 +20,31 @@ class UserRepository
         $query = $this->user->newQuery();
 
         // Menghindari N+1 Problem, jadi menggunakan eager loading
-        if (!empty($params['with'])) {
+        if (! empty($params['with'])) {
             $query->with($params['with']);
         }
 
-        // Exact filters (DRY dengan loop) 
+        // Exact filters (DRY dengan loop)
         $exactFilters = ['email', 'role', 'username'];
-        
+
         foreach ($exactFilters as $field) {
-            if (!empty($params[$field])) {
+            if (! empty($params[$field])) {
                 $query->where($field, $params[$field]);
             }
         }
 
         // General Filter
-        if (!empty($params['search'])) {
+        if (! empty($params['search'])) {
             $query->where(function ($q) use ($params) {
-                $q->where('name', 'like', "%{$params['search']}%")
-                  ->orWhere('email', 'like', "%{$params['search']}%");
+                $q->where('username', 'like', "%{$params['search']}%")
+                    ->orWhere('email', 'like', "%{$params['search']}%");
             });
         }
 
         // Sorting
         $sortBy = $params['sort_by'] ?? 'created_at';
         $sortOrder = $params['sort_order'] ?? 'desc';
-        
+
         $query->orderBy($sortBy, $sortOrder);
 
         return $query;
@@ -53,7 +53,7 @@ class UserRepository
     public function save(User $user): User
     {
         $user->save();
-        
+
         return $user->fresh();
     }
 }
