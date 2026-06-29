1dbb5c5659b597b8ba607983e9304009f8987381
pikio <freakyfreakzz21@gmail.com>
2026-06-29 20:15:08 +0700

    chore: update fixing space

--- app/Enums/FamilyRelation.php
+++ app/Enums/FamilyRelation.php
@@ -4,13 +4,13 @@ namespace App\Enums;
 
 enum FamilyRelation: string
 {
-    Case Child = 'child';
-    Case Wife = 'wife';
-    Case HeadFamily = 'head';
+    case Child = 'child';
+    case Wife = 'wife';
+    case HeadFamily = 'head';
 
     public function label(): string
     {
-        return match($this) {
+        return match ($this) {
             self::Child => 'Anak',
             self::Wife => 'Istri',
             self::HeadFamily => 'Kepala Keluarga',
