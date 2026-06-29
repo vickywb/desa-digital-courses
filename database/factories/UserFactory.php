d22511912caee54714631e60d092ee40252f923d
pikio <freakyfreakzz21@gmail.com>
2026-06-29 20:59:10 +0700

    chore: update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'role' => 'head_of_family',
+            'is_active' => true,
             'remember_token' => Str::random(10),
         ];
     }
